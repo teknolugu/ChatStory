ba3571a2c0215364c5adcec8e3fa344d4fbeebf9
Ahmad Kholid <kholid060@gmail.com>
2021-03-07 09:59:17 +0800

    add tooltip directive

--- src/main.js
+++ src/main.js
@@ -4,8 +4,13 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import ui from './lib/ui';
+import VTooltip from './directives/VTooltip';
 import 'vue-toastification/dist/index.css';
 import './assets/css/tailwind.css';
 import './assets/css/style.css';
 
-createApp(App).use(store).use(router).use(ui).use(Toast).mount('#app');
+const app = createApp(App);
+
+app.directive('tooltip', VTooltip);
+
+app.use(store).use(router).use(ui).use(Toast).mount('#app');
